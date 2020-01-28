4cb1bc4c8967bc9f03aa797a26d6cd332b474fa3
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-01-28 23:12:35 +0100

    64 @ExceptionHandler(ConstraintViolationException.class)

--- src/main/java/pl/krzys/kukla/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/pl/krzys/kukla/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -4,18 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.krzys.kukla.msscbrewery.service.v2.BeerServiceV2;
 import pl.krzys.kukla.msscbrewery.web.model.v2.BeerDtoV2;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -38,7 +34,7 @@ public class BeerControllerV2 {
 
     @PostMapping //POST - creating new Beer
     //@RequestBody binds property from request to BeerDto object
-    public ResponseEntity<BeerDtoV2> handlePost(@RequestBody BeerDtoV2 beerDto) {
+    public ResponseEntity<BeerDtoV2> handlePost(@Valid @RequestBody BeerDtoV2 beerDto) {
         BeerDtoV2 savedBeer = beerServiceV2.saveBeer(beerDto);
 
         //here ResponseEntity should have location header on it
@@ -54,7 +50,7 @@ public class BeerControllerV2 {
     // is idempotent - we can call that multiple times - will be affect only first time
     @PutMapping(value = "/{beerId}")
     //beerId should be passed here as above to be read only to not allow client update that property for BeerDto object
-    public ResponseEntity handleUpdate(@PathVariable UUID beerId, @RequestBody BeerDtoV2 beerDto) {
+    public ResponseEntity handleUpdate(@PathVariable UUID beerId, @Valid @RequestBody BeerDtoV2 beerDto) {
         beerServiceV2.updateBeer(beerId, beerDto);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
@@ -67,6 +63,16 @@ public class BeerControllerV2 {
 
         //we could make it as return new ResponseEntity(HttpStatus.NO_CONTENT) it allows us to make more control of that
         // for example to some headers with location etc.
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException ex) {
+        List<String> errors = new ArrayList<>();
+        ex.getConstraintViolations().forEach(constraintViolation -> {
+            errors.add(constraintViolation.getPropertyPath() + " " + constraintViolation.getMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }

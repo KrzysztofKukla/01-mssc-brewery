10d594680132b757a3247b3199f1fe31026b32ce
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-01-29 21:27:39 +0100

    66 Validation for CustomerDto

--- src/main/java/pl/krzys/kukla/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/pl/krzys/kukla/msscbrewery/web/controller/CustomerController.java
@@ -4,18 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.krzys.kukla.msscbrewery.service.CustomerService;
 import pl.krzys.kukla.msscbrewery.web.model.CustomerDto;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -33,7 +29,7 @@ public class CustomerController {
     }
 
     @PostMapping
-    public ResponseEntity<CustomerDto> handlePost(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<CustomerDto> handlePost(@RequestBody @Valid CustomerDto customerDto) {
         CustomerDto savedCustomerDto = customerService.saveCustomer(customerDto);
 
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -44,7 +40,7 @@ public class CustomerController {
 
     @PutMapping("/{customerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void handlePut(@PathVariable UUID customerId, @RequestBody CustomerDto customerDto) {
+    public void handlePut(@PathVariable UUID customerId, @RequestBody @Valid CustomerDto customerDto) {
         customerService.updateCustomer(customerId, customerDto);
     }
 
@@ -52,6 +48,15 @@ public class CustomerController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID customerId) {
         customerService.deleteCustomerById(customerId);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> validationErrorHandler(ConstraintViolationException ex) {
+        List<String> errors = new ArrayList<>();
+        ex.getConstraintViolations().forEach(i -> {
+            errors.add(i.getPropertyPath() + " " + i.getMessage());
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
